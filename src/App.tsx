735a5878384b55ee84615a890fd0553746f0d9ce
SHIBINSHA <shibin24888@gmail.com>
2024-10-11 12:24:36 +0530

    Initial setup made

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+// src/App.tsx
+import React from 'react';
+
+const App: React.FC = () => {
+    return (
+        <div className="min-h-screen flex items-center justify-center bg-gray-100">
+            <div className="text-center">
+                <h1 className="text-4xl font-bold mb-4">Welcome to FOSS Essentials</h1>
+                <p className="text-lg text-gray-700">Your go-to resource for Free and Open Source Software!</p>
+            </div>
+        </div>
+    );
+};
+
+export default App;
